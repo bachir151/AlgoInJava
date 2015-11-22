1468f5e43aa2abc675af8dbb5ced952ae5e3b514
bachir151 <elbachir90@gmail.com>
2015-11-23 00:15:03 +0100

    GitAddFile

---
+++ ajoute_fichier.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -lt 0 ]
+then
+   echo ""
+   echo "Usage: ajoute_fichier.sh [fichier1] [fichier2]  [commit] "
+   echo ""
+ exit 1
+fi
+
+#cp $1 .
+git add $1 
+git commit -m "$2"
+git push 
+
